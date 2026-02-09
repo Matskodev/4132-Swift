b6e4f87df1dd88b5f420e620792c339bf131a05c
SBabchukTM <babchuk.serhii@trident-media.agency>
2026-02-09 13:59:21 +0200

    init

---
+++ Pods/Target Support Files/Pods-TaskRoster/Pods-TaskRoster-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TaskRoster : NSObject
+@end
+@implementation PodsDummy_Pods_TaskRoster
+@end
---
+++ TaskRoster/App/TaskRosterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskRosterApp.swift
+//  TaskRoster
+//
+//  Created by Serhii Babchuk on 09.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct TaskRosterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            TRLaunchView()
+        }
+    }
+}
